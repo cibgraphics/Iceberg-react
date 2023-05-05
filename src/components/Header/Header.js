0c590f4fc6bc2b52c9e87bba1c636bcf7f3816ac
Christopher Bishop <cbishop@cibgraphics.com>
2023-05-05 17:40:49 -0600

    logo

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,7 +3,9 @@ import HeaderNav from './HeaderNav';
 const Header = () => {
   return (
     <header className="app-header">
-      <div className="logo" />
+      <div className="logo">
+        <img src="/assets/images/logos/main-logo.svg" />
+      </div>
       <HeaderNav />
     </header>
   )
