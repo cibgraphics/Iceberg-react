902bd84668d78f774995bb2038a2422603417478
Christopher Bishop <cbishop@cibgraphics.com>
2023-05-05 17:31:49 -0600

    init commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import HeaderNav from './HeaderNav';
+
+const Header = () => {
+  return (
+    <header className="app-header">
+      <div className="logo" />
+      <HeaderNav />
+    </header>
+  )
+}
+
+export default Header;
---
+++ src/components/Header/HeaderNav.js
@@ -0,0 +1,16 @@
+const HeaderNav = () => {
+  return (
+    <nav className="app-nav">
+      <ul className="list-unstyled">
+        <li>
+          <a href="#0">Settings</a>
+        </li>
+        <li>
+          <a href="#0">Logout</a>
+        </li>
+      </ul>
+    </nav>
+  )
+}
+
+export default HeaderNav;
