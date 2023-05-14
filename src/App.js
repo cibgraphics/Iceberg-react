e0020c627000236dfd21642a5a5feae14d2bdff5
Christopher Bishop <cbishop@cibgraphics.com>
2023-05-14 16:54:05 -0600

    fake data table for projects

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import { Routes, Route} from 'react-router-dom';
-import { initializeApp } from "firebase/app";
 
 import RootLayout from './components/layout/RootLayout';
 import AuthLayout from './components/layout/AuthLayout';
@@ -8,16 +7,6 @@ import Home from './pages/Home';
 import Dashboard from './pages/app/Dashboard';
 import Settings from './pages/app/Settings';
 import Login from './pages/auth/Login';
-
-const firebaseConfig = {
-  apiKey: "AIzaSyDE38uy_h3mtmr3tx5__l_sfH3kgvogrHE",
-  authDomain: "iceberg-2f78e.firebaseapp.com",
-  projectId: "iceberg-2f78e",
-  storageBucket: "iceberg-2f78e.appspot.com",
-  messagingSenderId: "507528991944",
-  appId: "1:507528991944:web:ef132584f57602d29f41fd",
-  measurementId: "G-SE1V0ZKYNG"
-};
 
 
 function App() {
@@ -37,40 +26,6 @@ function App() {
       </Routes>
     </div>
   )
-}
-
-export default App;
-
-
-
-
-import {Link} from 'react-router-dom';
-import Header from './components/Header/Header';
-
-
-
-
-const firebaseConfig = {
-  apiKey: "AIzaSyDE38uy_h3mtmr3tx5__l_sfH3kgvogrHE",
-  authDomain: "iceberg-2f78e.firebaseapp.com",
-  projectId: "iceberg-2f78e",
-  storageBucket: "iceberg-2f78e.appspot.com",
-  messagingSenderId: "507528991944",
-  appId: "1:507528991944:web:ef132584f57602d29f41fd",
-  measurementId: "G-SE1V0ZKYNG"
-};
-
-
-
-
-function App() {
-  return (
-    <div className="App">
-      <Header />
-      
-      <div>Logout</div>
-    </div>
-  );
 }
 
 export default App;
