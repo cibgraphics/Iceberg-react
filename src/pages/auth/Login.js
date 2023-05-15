d0d60e7f3fc7ed928a877c248853696a131b5780
Christopher Bishop <cbishop@cibgraphics.com>
2023-05-15 15:18:19 -0600

    temp code

--- src/pages/auth/Login.js
+++ src/pages/auth/Login.js
@@ -1,16 +1,16 @@
-import { initializeApp } from "firebase/app";
+// import { initializeApp } from "firebase/app";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyDE38uy_h3mtmr3tx5__l_sfH3kgvogrHE",
-  authDomain: "iceberg-2f78e.firebaseapp.com",
-  projectId: "iceberg-2f78e",
-  storageBucket: "iceberg-2f78e.appspot.com",
-  messagingSenderId: "507528991944",
-  appId: "1:507528991944:web:ef132584f57602d29f41fd",
-  measurementId: "G-SE1V0ZKYNG"
-};
+// const firebaseConfig = {
+//   apiKey: "AIzaSyDE38uy_h3mtmr3tx5__l_sfH3kgvogrHE",
+//   authDomain: "iceberg-2f78e.firebaseapp.com",
+//   projectId: "iceberg-2f78e",
+//   storageBucket: "iceberg-2f78e.appspot.com",
+//   messagingSenderId: "507528991944",
+//   appId: "1:507528991944:web:ef132584f57602d29f41fd",
+//   measurementId: "G-SE1V0ZKYNG"
+// };
 
-const app = initializeApp(firebaseConfig);
+// const app = initializeApp(firebaseConfig);
 
 const Login = () => {
   return (
